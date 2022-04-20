4ab47cd3f36a11443775930c490366fe5912d078
ersan80 <ersan.kuleli80@gmail.com>
2022-04-20 14:37:02 +0300

    React-Redux-News-Project

---
+++ src/redux/store/index.jsx
@@ -0,0 +1,21 @@
+import { createStore,applyMiddleware } from "redux";
+import rootReducer from "../reducer";
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from "redux-thunk"
+
+let store;
+if (process.env.NODE_ENV==="development"){
+    store = createStore(
+        rootReducer,
+        composeWithDevTools(applyMiddleware(thunk))
+        )
+}else{
+    store = createStore(
+        rootReducer,applyMiddleware(thunk)
+        )
+
+}
+
+
+
+export default store;
